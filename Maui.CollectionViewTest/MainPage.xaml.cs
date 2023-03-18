4751bc66e02d9d87cefe7a86b91354245f67ad7c
강준 <keisoft.kjun@gmail.com>
2023-03-18 22:34:49 +0900

    초기 커밋.

---
+++ Maui.CollectionViewTest/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Maui.CollectionViewTest;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        List<string> list = new List<string>();
+
+        for (int i = 1; i <= 25; i++)
+        {
+            list.Add(i.ToString());
+        }
+
+        this.listview.ItemsSource = list;
+    }
+}
+
---
+++ Maui.CollectionViewTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Maui.CollectionViewTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
